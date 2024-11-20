dfc80609c1376cbde25d2f353216cd12ebeb1a1c
jordanjordanjordan0 <jhill011@boyslatin.net>
2024-11-20 14:35:07 +0000

    Functions11/20

---
+++ functions.py
@@ -0,0 +1,65 @@
+# what is a function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(name)
+
+number= int(input('please provide a number'))
+print( 23*number)
+
+# User Defined Functions- Custom functions written by engineers
+
+# Function Syntax - how it is written
+
+def sandwichIntructions()
+    print('step 1. get 2 pieces of bread.')
+    print('step 2. putting ingredients inside of bread')
+    print('step 3 put bread with ingredients together')
+
+
+
+
+
+
+
+# 11/20/24
+
+# functions using arithmetic operators
+
+def depositMoney(x)
+    checkingAccount = 100   
+    print('money has been deposited successfully')
+    print('new checking account balance is below: ')
+    print(X + checkAccount)
+depositMoney(35)
+depositMoney(200)
+depositMoney(95)
+
+def withdrawMoney(x)
+    print('money has been withdrawn successfully')
+    print('new checking account balance below')
+    print(X - checkAccount)
+withdrawMoney(40)
+withdrawMoney(300)
+withdrawMoney(85)
+
+def checkAcountBalance(x)
+    print('money avalible')
+    print('Amount in checking account')
+    print(checkAccount)
+
+
+
+def transferMoney()
