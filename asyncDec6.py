0dcc79af27a03b582aec5eafcbf06f2a5dea3d21
jordanjordanjordan0 <jhill011@boyslatin.net>
2024-12-10 14:18:37 +0000

    ASYNC DEC 6

---
+++ asyncDec6.py
@@ -0,0 +1,22 @@
+QUESTION 1
+
+
+def reverseWord(word):
+    txt = word[:;-1]
+    print(txt)
+
+reverseWord('pizza party')
+
+
+
+QUESTION 2
+
+def landmarkByState(state):
+    if state == 'PA':
+        print(' landmark in Pa is the liberty bell in philly.')
+    if state == NY
+        print('A landmark in NY is the statue of liberty in Nyc.')
+    else:
+        print('The info on this states landmarks are unavailable.')
+
+landmarkByState('NY')        
