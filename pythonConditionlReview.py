fde6f10cf738361a0305a9a704e9041efa0deeba
jordanjordanjordan0 <jhill011@boyslatin.net>
2024-12-11 14:36:05 +0000

    ConditionalReview

---
+++ pythonConditionlReview.py
@@ -0,0 +1,37 @@
+# functions are just instructions
+# for the computer to know what to do
+# with data
+
+# conditional statements use the if/ else
+# keywords to make decisions and outcomes
+# based on data
+
+def welcomeMsgByTime(number,time):
+    if (time== 'am'):
+       print('good morning')
+       print(number + time)  
+    elif time == 'pm'
+        print('good evening')
+        print(str(number) + time)
+    elif number > 12:
+        print('sorry number cant be greater than 12')
+    else:
+        print('sorry, did not understand your input')
+
+# welcomeMsgByTime(1,'pm')
+
+
+def letterByNumber(letter,number):
+    if (B== '80'):
+        print('Grade')
+        print(Letter + number)
+
+
+
+
+
+
+
+def numericalGrade(grade, letter):
+    if grade == '98':
+        print 'A'
