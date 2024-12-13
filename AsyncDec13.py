ddf8d9c8fa05f13b3dbb9b725cea70221fa2a2e8
jordanjordanjordan0 <jhill011@boyslatin.net>
2024-12-13 14:34:02 +0000

    AsyncWorkDec13

---
+++ AsyncDec13.py
@@ -0,0 +1,34 @@
+Question 2
+
+
+def priceByAge(Age,Number)
+     If Age == 20 - 64
+     Print '15.00'
+     If Age == 16 - 19
+     Print '10.00'
+     If Age == 1 - 10
+     Print '5.00'
+     If Age == 65 - 99
+     Print '5.00'
+
+
+
+QUESTION 3
+
+
+
+    def discountByMembership(Membership,Discount)
+                 SuperShopper == 10%
+                 MegaShopper == 15%
+                 UltraShopper == 20%
+                 Print(100 - 10%)
+                 print('Congratulations you saved 10 dollar)
+
+
+
+
+QUESTION 1  
+
+    def MessageoNumberInput(PositveNumber,PostiveMessage)
+                           (NegativeNumber,NegativeMessage)
+                           Print
