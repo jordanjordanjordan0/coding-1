d0f0b4918400b00de69d58f7a78be0493ff4acc1
jordanjordanjordan0 <jhill011@boyslatin.net>
2025-01-13 14:10:49 +0000

    AsyncJan10 Review

---
+++ unit 4/AsyncJan10.py
@@ -0,0 +1,31 @@
+# Question 1
+
+variable = 'jordan'
+newlist =[1,2,34,55,]
+produceFood =[0,12.35, true, variable,newlist]
+
+produceFood = ['apples','garlic','bell peppers','bananas','spinach']
+frozenFood = ['pizza','string beans','hashbrown','TV dinners',frozen fruit]
+
+# Question 2
+
+numbers = [1,30,39,50,293,100]
+
+
+
+
+# Question 3
+
+def multiplyByNumber(x):
+    result = x * numbers[4]
+    print(result)
+
+multiplyByNumber(1)
+
+
+def traveledPlaces():
+
+    place = input(''where have you traveled to? '')
+    print(place)
+
+traveledPlaces()
---
+++ unit 4/pythonLoops.py
@@ -0,0 +1,19 @@
+
+
+
+
+def titleBySchoolYear(year)
+    
+    
+    
+    
+    year = int(input('how many years of college do you have?'))
+    if year == 0:
+        print(collegeTitles[0])
+    elif year== 1:
+        print(collegeTitles[1])
+    elif year== 2:
+        print(collegeTitles[3])
+    elif year== 3:
+        print(collegeTitles[4])
+    elif year== 4:
