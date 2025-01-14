a1ac8ac1b3443957435aac8a27b5c5ff08c30f39
jordanjordanjordan0 <jhill011@boyslatin.net>
2025-01-14 14:36:09 +0000

    Jan 14 Lesson

---
+++ unit 4/listFunctions.py
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+skii_trip_items.append('gloves')
+skii_trip_items.append('thick socks')
+print(skii_trip_items)
+
+
+
+
+
+
+skii_trip_items.insert(2.'goggles')
+print(skii_trip_items)
+
+
+skii_trip_items.pop()
+print(skii_trip_items)
+
+
+
+skii_trip_items.remove('snow boots')
+print(skii_trip_items)
+
+
+skii_trip_items.clear()
+print(skii_trip_items)
+
+
+
+
+def clothingBySeason():
+    myClothes = ['tee-shirt','shorts','  ']
+
+
+
+
+
+
+
+    if clothingSelection == 'tee-shirt'
+
+def favoriteResturant():
+    resturants =['wendys']
