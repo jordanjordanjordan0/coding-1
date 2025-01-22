020de46bab85d9083631ff82936126e439d4463d
jordanjordanjordan0 <jhill011@boyslatin.net>
2025-01-22 14:35:56 +0000

    Jan22 Loops

---
+++ asyncActivityJan17.py
@@ -0,0 +1,39 @@
+campingSupplies
+
+
+
+
+
+# 1
+campingSupplies.reverse()
+
+print(campingSupplies)
+
+# 2
+campingSupplies.append('water bottle')
+# of the list
+print(campingSupplies)
+
+campingSupplies.insert('first aid kit')
+
+print(campingSupplies)
+
+
+campingFood = 
+
+
+
+
+
+
+
+
+
+
+
+
+# 4. remove flashlight from list and replace it with campfire kit
+campingSupplies.remove('flash light')
+campingSupplies.insert(2, 'campfire kit')
+
+print(campingSupplies)
---
+++ whileLoops.py
@@ -0,0 +1,35 @@
+# Loops - A construct in programming 
+# where instructions will repeat over and 
+# over until a specific condition condtion is met
+
+# While loop- is special type of loop where
+# # instructions will repat themselves
+# so long as a condition is true
+
+x = 2
+while x ==2:
+    print('this essage will repeat forever. ')
+
+def passwordLoops():
+    correctPw= '123abc'
+    userPw=input('please enter your password:')
+    while userPw != correctPw:
+        print('incorrect pw. Try again')
+        userPw=input('please enter your password:')
+    if userPw == correctPw
+
+
+
+
+# passwordLoops()
+#
+def inentoryLoop()
+    userinvertory =[]
+    pickupItem= input('what item are you picking')
+    while userInventory.len() == 0:
+        userInventory.append(pickupItem)
+        print('these are the items in your bag: ')
+        print(userInventory)
+        pickupItem= input('what item are you picking up?: ')
+
+inventoryLoop()
