b6dcc012e54eb3c73af90d541e780d34a7725772
jordanjordanjordan0 <jhill011@boyslatin.net>
2025-02-03 14:11:36 +0000

    AsyncJan31

---
+++ AsyncJan31.py
@@ -0,0 +1,17 @@
+
+
+
+"2 Averaging out the grade seems tricky, but this is a concept in arithmetic"
+
+"3"
+" Student needs to enter numerical grade = integer/floats"
+" We need to give the user back a letter grade.= ex. 80= B"
+" Program needs to repeat asking the user to enter a number = loop"
+" Each time it repeats it take the new number and averages the total"
+
+"4 Yes, the prompt tells us that a user needs to enter a number."
+"the number represents the numerical grade"
+
+"5 Yes, we need to give back the letter grade based on the number"
+"entered"
+
