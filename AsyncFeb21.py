48387d9ec28b6b446afad857b4b762f4ad224b00
jordanjordanjordan0 <jhill011@boyslatin.net>
2025-02-21 14:27:11 +0000

    AsyncFeb21

---
+++ AsyncFeb21.py
@@ -0,0 +1,10 @@
+# Football Touchdown Scorer - Doing this because i love Football - Question 1
+
+# It is a RPG(Role Playing Game) sports game - Question 2
+
+# The player will have to get around the defenders and score. If the player is touch by the Defender than he will be tackled. There will be a set amount of time in the game. - Question 3
+
+# Retro Bowl is the only inspriation for my game. It has the mechanics and design that i want to base mine off of. It is an amazing and well put together game that Hundreds of thousands play. - Question 4
+
+# Some Mechanics will be the scoring system. Another one is how they move around the field. Another one is how many player you can have on the field. As well as team colors.
+# Last one is the scoring when the player reaches a certain part of the field(Endzone/Touchdown) - Question 5
